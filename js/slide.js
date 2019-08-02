7d7ed8643b9adb145c19f5f7428f43562b79ed6f
pudou@stdnt-cl <pudouotaku@gmail.com>
2019-08-02 20:35:16 -0300

    First central class principles and logic

--- js/slide.js
+++ js/slide.js
@@ -3,5 +3,15 @@ export default class Slide {
       this.slide = document.querySelector(sld); 
       this.wrapper = document.querySelector(wrppr);
    }
-
+   onStart(event) {
+      event.preventDefault();
+   }
+   addSlideEvents() {
+      this.wrapper.addEventListener('mouseon', this.onStart);
+      console.log(event);
+   }
+    init() {
+      this.addSlideEvents();
+      return this; 
+    }
 }
